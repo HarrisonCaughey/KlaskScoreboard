60c487c94881857d13097d5ecf3140b50cd530fe
Harrison Caughey <harrison.caughey@adscale.co.nz>
2022-07-21 10:31:25 +1200

    Added env and index.js changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {App} from './client/app.js'
 import {HashRouter, BrowserRouter as Router} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
